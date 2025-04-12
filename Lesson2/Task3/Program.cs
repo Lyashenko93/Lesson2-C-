079128831ed88eae342c1a95d2215e6ae514bb91
Ирина Ляшенко <tawfuiofmv6s8z17h@mail.ru>
2025-04-12 23:44:20 +0300

    add Lesson2:TASK3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = {2, 8, 6 , 10, 7 ,3, 1, 22, 5, 9};
+int i = 0;
+
+while(i<n)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i+1;
+}
